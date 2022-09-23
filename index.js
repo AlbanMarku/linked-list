3b82b4e71443b68de00931c8e895429caecdda0c
AlbanMarku <albanmarku@outlook.com>
2022-09-23 20:02:27 +0100

    Setup eslint.

---
+++ index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
+const message = "hiya";
+
+function runThis() {
+    for (let i = 0; i < 3; i++) {
+        console.log(message);
+    }
+}
+
+runThis();
