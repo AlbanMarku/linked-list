1267574d07e098cf75fce848caea02b95e764fe0
AlbanMarku <albanmarku@outlook.com>
2022-09-26 20:00:04 +0100

    insert last.

--- List.js
+++ List.js
@@ -8,6 +8,22 @@ class LinkedList {
 
     insertFirst(data) {
         this.head = new Node(data, this.head);
+        this.size++;
+    }
+
+    insertLast(data) {
+        const node = new Node(data);
+
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+            this.size++;
+        }
     }
 
     printList() {
@@ -23,5 +39,5 @@ class LinkedList {
 const ll = new LinkedList();
 ll.insertFirst(2);
 ll.insertFirst(3);
-ll.insertFirst("last");
+ll.insertLast(4);
 ll.printList();
